6509747a26acc830fdc049d09b33955af60f052a
rubo <ruluy0205@163.com>
2024-07-29 20:24:14 +0800

    finish lock bcache

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,12 +1,16 @@
 struct buf {
   int valid;   // has data been read from disk?
-  int disk;    // does disk "own" buf?
-  uint dev;
+  int disk;    // does disk "own" buf? 缓冲区内容是否已交给磁盘
+  // 缓存块的标识
+  uint dev;    
   uint blockno;
+
   struct sleeplock lock;
-  uint refcnt;
+  uint refcnt;    // 引用计数
   struct buf *prev; // LRU cache list
   struct buf *next;
   uchar data[BSIZE];
+
+  uint timestamp; // 时间戳
 };
 
