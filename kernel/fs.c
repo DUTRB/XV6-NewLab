3c16e0d944779db983a191cb6fbd58227ad0016f
rubo <ruluy0205@163.com>
2024-08-01 16:06:52 +0800

    finish bigfile

--- kernel/fs.c
+++ kernel/fs.c
@@ -374,6 +374,7 @@ iunlockput(struct inode *ip)
 
 // Return the disk block address of the nth block in inode ip.
 // If there is no such block, bmap allocates one.
+// bn 为要查找的块编号
 static uint
 bmap(struct inode *ip, uint bn)
 {
@@ -395,6 +396,33 @@ bmap(struct inode *ip, uint bn)
     a = (uint*)bp->data;
     if((addr = a[bn]) == 0){
       a[bn] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
+  bn -= NINDIRECT;
+
+  if(bn < NDINDIRECT){
+    int level2_idx = bn / NADDR_PER_BLOCK;  // 二级间接块位置
+    int level1_idx = bn % NADDR_PER_BLOCK;  // 一级间接块的位置
+
+    if((addr = ip->addrs[NDIRECT + 1]) == 0){
+      ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+    }
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+
+    if((addr = a[level2_idx]) == 0){
+      a[level2_idx] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+
+    bp = bread(ip->dev, addr);
+    a = (uint*)bp->data;
+    if((addr = a[level1_idx]) == 0){
+      a[level1_idx] = addr = balloc(ip->dev);
       log_write(bp);
     }
     brelse(bp);
@@ -431,6 +459,31 @@ itrunc(struct inode *ip)
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
   }
+
+  /*  code here */
+  struct buf* bp1;
+  uint* a1;
+
+  if(ip->addrs[NDIRECT + 1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a = (uint*)bp->data;
+    for(i = 0; i < NADDR_PER_BLOCK; i++){
+      if(a[i]){
+        bp1 = bread(ip->dev, a[i]);
+        a1 = (uint*)bp->data;
+        for(int j = 0; j < NADDR_PER_BLOCK; j++){
+          if(a1[j])
+            bfree(ip->dev, a1[j]);
+        }
+        brelse(bp1);
+        bfree(ip->dev, a[i]);
+      }
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
+  }
+  /*  code end  */
 
   ip->size = 0;
   iupdate(ip);
